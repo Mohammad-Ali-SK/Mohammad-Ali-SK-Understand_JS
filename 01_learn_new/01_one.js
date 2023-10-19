ed806aed0bae275ad3aae621a28b030d75d4b54e
Mohammad Ali Sk <mohammadalisk050@gmail.com>
2023-10-19 11:20:16 +0530

    Day_3

---
+++ 01_learn_new/01_one.js
@@ -0,0 +1,34 @@
+
+
+
+// video ---------------26;
+
+// if(true){
+//     console.log('Mohammad')
+// };
+
+// if(false){
+//     console.log('Mohammad Ali')
+// }
+
+// console.log(3 != 3);   // sotti hole mittha er mittha hole sotti =!
+
+
+// console.log(2 == "2");
+
+// const useremail = "";
+
+// if(useremail){
+//     console.log("Got user email")-
+// }else{
+//     console.log("Don't have a account")
+// }
+
+// false value -----------------------------0.1)
+
+// false, 0 , -0, BigInt 0n, "", null, undefined, NaN
+
+// Truthy Value---------------------------0.2)
+
+// "0", 'false', "  ", [], {}, function(){};
+
---
+++ 03_Basic++/02_scope.js
@@ -0,0 +1,55 @@
+// var c = 300;
+// if(true){
+//     let a = 8;
+//     // const b = 6;
+//     var c = 99;
+//     console.log("Inner" , a);
+
+// }
+
+// // console.log(a);
+// // console.log(b);
+// console.log(c);
+
+
+// function one(){
+//     const username = "Mohammad"
+
+//     function two(){
+//         const website = 'Youtube'
+//         console.log(`{this.username}`);
+
+//     }
+
+//     console.log(website);
+//      two();
+// }
+
+
+// one()
+
+
+
+
+// Arro Function in JS_____!!!
+
+// function cha(){
+//     console.log(this)
+// // }
+
+// const myfunc = e => {
+//     console.log(e);
+//     console.log(this)
+// };
+
+// myfunc('Mohammad ALi Sk');
+
+
+
+// Immediatelv Invoked Function Expressions (IIFE)
+
+// ((name) => {
+//     console.log(name)
+// })('Mohammad Ali sk');
+
+
