f0fa161da16cbb56f4481dc88c637b3bba70fe66
Mohammad Ali Sk <mohammadalisk050@gmail.com>
2023-10-22 19:51:53 +0530

    Day_6

--- 04_Day_JS/01_script.js
+++ 04_Day_JS/01_script.js
@@ -70,4 +70,56 @@ console.log('Hello');
 
 // // ans.catch(() => {
 // //     console.log("Kuch Nehi")
-// // })
+// /
+
+
+// Map , Filter , Reduse in Js
+
+
+// const codding = ["js", "rubay", "java", "python"];
+
+// const values = codding.forEach((item) => {
+//     console.log(item);
+//     // return item;
+// });
+
+// console.log(values);
+
+// const myNums = [1,3,2,4,5,6,7,8,9,10];
+
+
+// const ans = myNums.filter((item) => {
+//     return item < 9;
+//     // console.log(item)
+// }
+// );
+
+// console.log(ans);
+
+// const myNums = [1,3,2,4,5,6,7,8,9,10];
+
+// const ans = myNums.map((item) => {
+//     return item + 10;
+// })
+
+// console.log(ans);
+
+
+// const ans = myNums.map((num) => {
+//     return num + 10;
+// }).map((item) => {
+//     return item + 1;
+// });
+
+// console.log(ans);
+
+
+// const myNums = [1,2,3];
+// const ans = myNums.reduce((acc, currv) => {
+//     console.log(`The ${acc} and ${currv}`)
+//     return acc + currv;
+// });
+
+// console.log(myNums);
+
+ 
