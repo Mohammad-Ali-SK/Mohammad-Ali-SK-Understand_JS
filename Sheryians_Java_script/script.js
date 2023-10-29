f14be6d3aacc2894ed917c4496901f00967598a8
Mohammad Ali Sk <mohammadalisk050@gmail.com>
2023-10-29 09:58:10 +0530

    Day-7

--- Sheryians_Java_script/script.js
+++ Sheryians_Java_script/script.js
@@ -1,3 +1,27 @@
 
-// Understand JavaScript
+// Understand JavaScript : sync and async functions !!
+console.log("Hello world 1");
+setTimeout(() => {
+    console.log("Hello ");
+}, 1000);
+console.log("Hello World 2");
 
+
+// Js is not asynchronous !
+
+// Event loop help to two way conversions  ------ inportant question for interview  !!
+
+// Understand Promise ---!!
+
+const ans = new Promise((resolve, reject) => {
+    if(false){
+        return resolve();
+    }else{
+        return reject();
+    }
+
+});
+
+ans
+.then(() => console.log("success"))
+.catch(() => console.log("error"))
---
+++ Understand DOM/script.js
@@ -0,0 +1,28 @@
+// Understanding DOM in the js
+
+document.querySelector("h1").innerHTML = "Change inner HTML";
+
+// function create(){
+//     var h1 = document.createElement("h2");
+//     h1.innerHTML = "Hello";
+//     document.body.appendChild(h1);
+//     h1.style.color = "red";
+//     h1.style.fontSize = "109px";
+  
+// };
+
+    var bulb = document.createElement("#bulb");
+    var button = document.querySelector("button");
+    button.addEventListener("click", () => {
+        bulb.style.backgroundColor = "yellow";})
+    
+
+
+
+
+
+
+
+
+// create();
+
