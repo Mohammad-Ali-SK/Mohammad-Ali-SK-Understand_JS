143a68b212dc838d5960e08573d86c84f0fc5b3c
Mohammad Ali Sk <mohammadalisk050@gmail.com>
2023-11-18 22:54:13 +0530

    Apna_Collage_02

---
+++ Apna_Collage/02.js
@@ -0,0 +1,23 @@
+// Operator's in JS
+console.log("Mohammad ALi SK")
+
+// Arthimetic Operators
+// +,-,*,/,
+
+// Modulus ---> %;
+
+// Exponention ---> **, ***, ;
+
+// Increment -----> ++
+
+// Decrement ---> --
+
+let a = 9;
+let b = 10;
+console.log(a || b);
+
+
+
+// Assignment Operators
+
+// =, +=, -=, *=, %=, **=
