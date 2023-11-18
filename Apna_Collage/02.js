a8704546d455613c4a33da282a9bffaa5df35225
Mohammad Ali Sk <mohammadalisk050@gmail.com>
2023-11-18 23:31:11 +0530

    Question_Apna_Collage

--- Apna_Collage/02.js
+++ Apna_Collage/02.js
@@ -14,10 +14,34 @@ console.log("Mohammad ALi SK")
 
 let a = 9;
 let b = 10;
-console.log(a || b);
+// console.log(a || b);
 
 
 
 // Assignment Operators
 
 // =, +=, -=, *=, %=, **=
+
+let age =  78;
+
+if(age%2 === 0){
+    console.log('You Enter !')
+}
+else{
+    console.log("NOOOOOOOOOOOOO")
+};
+
+// This called ternery operator ----!!
+
+console.log(age >= 18 ? "You Vote" : "Sorry");
+
+let scour = 70;
+if(scour >= 90 && scour <= 100){
+    console.log("A");
+}else if(scour >= 70 && scour <=89){
+    console.log("B");
+}else if(scour >= 50 && scour <= 69){
+    console.log("C")
+}else{
+    console.log("You are fail");
+}
