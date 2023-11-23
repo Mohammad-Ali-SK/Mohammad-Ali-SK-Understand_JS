17238461ee3e636e19638dd44600417aa8cee09e
Mohammad Ali Sk <mohammadalisk050@gmail.com>
2023-11-23 20:19:20 +0530

    Function & Method

--- Apna_Collage/Function_Method.js
+++ Apna_Collage/Function_Method.js
@@ -28,5 +28,35 @@ const countVol = (str) => {
     console.log(count);
 };
 
-// countVol("Hello");
+countVol("Hello");
+let arr = [3,33,45,67];
+arr.forEach((e,i) => {
+    console.log(i,e ** 2);
+});
 
+// Higer Order Function / Method **** is ForEach function
+
+// Some More important Array Methods 
+// Map();
+arr.Map();
+let arr2 = [1,2,3,4,5,6,7,8];
+const ma_p = arr2.filter((e) => {
+    return e < 3;
+});
+console.log(ma_p);
+
+// Reduce();
+
+let newArr = [1,2,34,5];
+const outPut = arr.reduce((res,curr) => {
+    return res + curr;
+});
+
+console.log(outPut);
+
+let n = prompt("Enter the number -");
+let arr4 = [];
+for(let i = 0; i <=n; i++){
+    arr4[i-1] = i;
+}
+console.log(arr4);
