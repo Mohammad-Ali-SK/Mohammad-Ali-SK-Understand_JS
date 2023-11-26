c4699af504e0e72d0d324ce2e6e46251908e5ccf
Mohammad Ali Sk <mohammadalisk050@gmail.com>
2023-11-26 10:08:24 +0530

    Lecture_7

---
+++ Apna_Collage/Dom.js
@@ -0,0 +1,28 @@
+// Dom Manipulation in JS
+
+// Properties:
+// tagname:
+// innerText:
+// innerHTML:
+// textContent:
+// div.getAttribute("id","class");
+// setAttribute("class","add new class");
+// node.style;
+
+// let el = document.createElement("div");
+
+let div = document.querySelector("div");
+console.dir(div);
+div.style.backgroundColor = "black";
+div.style.color = "#fff"
+
+// Dom _Part_2
+// DOM_Manipulation
+// Insert Element's
+// 1.0) node.append();
+// 2.0) node.prepend();
+// 3.0) node.before();
+// 4.0) node.after();
+
+// Delete Element 
+// node.remove();
