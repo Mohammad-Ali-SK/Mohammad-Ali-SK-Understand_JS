f4d04bda6f7e08cd673e8d0cfce692e390e53fb2
Mohammad Ali Sk <mohammadalisk050@gmail.com>
2023-11-26 10:27:59 +0530

    Lecture_7

--- Apna_Collage/Dom.js
+++ Apna_Collage/Dom.js
@@ -3,7 +3,7 @@
 // Properties:
 // tagname:
 // innerText:
-// innerHTML:
+// innerHTML: *****
 // textContent:
 // div.getAttribute("id","class");
 // setAttribute("class","add new class");
@@ -14,7 +14,13 @@
 let div = document.querySelector("div");
 console.dir(div);
 div.style.backgroundColor = "black";
-div.style.color = "#fff"
+div.style.color = "#fff";
+let el = document.createElement("button");
+el.innerText = "click me";
+// div.append(el);
+// div.prepend(el);
+// div.before(el);
+// div.after(el);
 
 // Dom _Part_2
 // DOM_Manipulation
@@ -23,6 +29,8 @@ div.style.color = "#fff"
 // 2.0) node.prepend();
 // 3.0) node.before();
 // 4.0) node.after();
+// 5.0) classList.add(); //classlist.remove();
 
 // Delete Element 
 // node.remove();
+// div.remove();
